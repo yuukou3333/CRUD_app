9e71de775db79e001fedb177e16c6a09280268c6
yuukou3333 <kuusou3333@gmail.com>
2020-01-25 21:46:20 +0900

    resources_appの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
     User.create(user_params)
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def edit
     @user = User.find(params[:id])
   end
